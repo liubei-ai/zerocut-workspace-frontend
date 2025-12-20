d81d95e89bd3ee1d9535f0cfc15a2e7f9b1175d4
lidongjie <lidongjies@gmail.com>
2025-12-20 10:56:48 +0800

    feat: 添加vite ssl插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 // Plugins
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import vue from '@vitejs/plugin-vue';
 import { viteMockServe } from 'vite-plugin-mock';
 import vuetify from 'vite-plugin-vuetify';
@@ -26,6 +27,14 @@ export default defineConfig({
       mockPath: 'src/mocks',
       enable: false,
     }),
+    basicSsl({
+      /** name of certification */
+      name: 'local',
+      /** custom trust domains */
+      domains: ['local-workspace.zerocut.cn'],
+      /** custom certification directory */
+      certDir: 'cert',
+    }),
   ],
   define: { 'process.env': {} },
   resolve: {
@@ -37,7 +46,6 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 8080,
     watch: {
       usePolling: true,
     },
@@ -62,7 +70,6 @@ export default defineConfig({
         api: 'modern-compiler',
         prependData: `@use 'src/styles/main.scss' as *;`,
       },
-      css: { charset: false },
     },
   },
 });
